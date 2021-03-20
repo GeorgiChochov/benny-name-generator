d3dda11141e31904d47cafa7908e9c8820f84a3d
Georgi Chochov <g.chochov@gmail.com>
2021-03-20 22:05:13 +0100

    WTF

--- src/main/scala/io/github/georgichochov/server/Main.scala
+++ src/main/scala/io/github/georgichochov/server/Main.scala
@@ -1,10 +1,12 @@
 package io.github.georgichochov.server
 
+import akka.Done
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Await, ExecutionContext, Future, Promise, blocking}
+import scala.concurrent.duration.Duration
 import scala.io.StdIn
 
 object Main {
@@ -18,11 +20,30 @@ object Main {
     println("Hello from Main")
     val port = sys.env.getOrElse("PORT", "8080").toInt
     val binding = Http().newServerAt("0.0.0.0", port).bind(getNewBenny)
-    StdIn.readLine()
+    Await.ready(
+      binding.flatMap(_ =>
+        waitForShutdownSignal()
+      ), // chaining both futures to fail fast
+      Duration.Inf
+    )
+
     binding.flatMap(_.unbind()).onComplete { _ =>
       println("So long")
       system.terminate()
     }
   }
-
+  protected def waitForShutdownSignal(
+  )(implicit ec: ExecutionContext): Future[Done] = {
+    val promise = Promise[Done]()
+    sys.addShutdownHook {
+      promise.trySuccess(Done)
+    }
+    Future {
+      blocking {
+        if (StdIn.readLine("Press RETURN to stop...\n") != null)
+          promise.trySuccess(Done)
+      }
+    }
+    promise.future
+  }
 }
