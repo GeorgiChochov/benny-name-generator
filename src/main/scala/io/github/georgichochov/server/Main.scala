797ebbc129599ebeb9e5c2966a5d618b01ffd70f
Georgi Chochov <g.chochov@gmail.com>
2021-03-20 21:56:10 +0100

    log on exit

--- src/main/scala/io/github/georgichochov/server/Main.scala
+++ src/main/scala/io/github/georgichochov/server/Main.scala
@@ -19,7 +19,10 @@ object Main {
     val port = sys.env.getOrElse("PORT", "8080").toInt
     val binding = Http().newServerAt("0.0.0.0", port).bind(getNewBenny)
     StdIn.readLine()
-    binding.flatMap(_.unbind()).onComplete(_ => system.terminate())
+    binding.flatMap(_.unbind()).onComplete { _ =>
+      println("So long")
+      system.terminate()
+    }
   }
 
 }
