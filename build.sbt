27278d5c4ed4c859c949375efacaa71ef9c70f9e
Georgi Chochov <g.chochov@gmail.com>
2021-03-27 23:03:30 +0100

    Let's see what heroku does

--- build.sbt
+++ build.sbt
@@ -9,7 +9,11 @@ val AkkaHttpVersion = "10.2.4"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
   "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
-  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "org.postgresql" % "postgresql" % "42.2.19",
+  "com.typesafe.slick" %% "slick" % "3.3.3",
+  "org.slf4j" % "slf4j-nop" % "1.7.25",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.3.3"
 )
 enablePlugins(JavaAppPackaging)
 mainClass in Compile := Some("io.github.georgichochov.server.Main")
--- src/main/scala/io/github/georgichochov/server/Main.scala
+++ src/main/scala/io/github/georgichochov/server/Main.scala
@@ -4,7 +4,14 @@ import akka.Done
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import io.github.georgichochov.model.Titles
+import slick.dbio.DBIO
+import slick.jdbc.JdbcBackend.Database
+import slick.lifted.TableQuery
+import slick.jdbc.PostgresProfile.api._
 
+import java.net.URI
+import java.util.UUID
 import scala.concurrent.{Await, ExecutionContext, Future, Promise, blocking}
 import scala.concurrent.duration.Duration
 import scala.io.StdIn
@@ -18,9 +25,44 @@ object Main {
     val getNewBenny = Routes.routes
 
     val dbUrl =
-      sys.env.getOrElse("DATABASE_URL", "jdbc:postgresql://localhost:5432/test")
-
+      sys.env.getOrElse(
+        "DATABASE_URL",
+        "postgres://test:test@127.0.0.1:5432/test"
+      )
     println(s"db url: $dbUrl")
+    val dbUri = new URI(dbUrl)
+    val username = dbUri.getUserInfo.split(":").head
+    val passwd = dbUri.getUserInfo.split(":").last
+    val connStr =
+      s"jdbc:postgresql://${dbUri.getHost}:${dbUri.getPort}${dbUri.getPath}"
+
+    val db = Database.forURL(
+      connStr,
+      user = username,
+      password = passwd,
+      driver = "org.postgresql.Driver"
+    )
+
+    val titles = TableQuery[Titles]
+
+    try {
+      val setupFuture = db.run(
+        DBIO.seq(
+          titles.schema.createIfNotExists,
+          titles += (UUID.randomUUID(), "Mr.")
+        )
+      )
+
+      Await.result(setupFuture, Duration.Inf)
+      println(
+        Await.result(
+          db.run(titles.filter(_.title === "Mr.").result),
+          Duration.Inf
+        )
+      )
+    } catch {
+      case e => e.printStackTrace()
+    }
     println("Hello from Main")
     val port = sys.env.getOrElse("PORT", "8080").toInt
     val binding = Http().newServerAt("0.0.0.0", port).bind(getNewBenny)
@@ -33,6 +75,7 @@ object Main {
 
     binding.flatMap(_.unbind()).onComplete { _ =>
       println("So long")
+      db.close()
       system.terminate()
     }
   }
